f62e8862487dbcbafc037258e7a354c653d7ce27
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2024-04-02 07:56:03 -0300

    Atualizado - aula 02/04/2023

--- MyFirstApp/app/src/main/java/br/edu/ifsuldeminas/mch/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/br/edu/ifsuldeminas/mch/myfirstapp/MainActivity.java
@@ -3,7 +3,9 @@ package br.edu.ifsuldeminas.mch.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,16 +15,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Acessar o botão
+        //Acessar elementos
         Button buttonLogin = findViewById(R.id.buttonLogin);
+        EditText userName = findViewById(R.id.editTextUser);
+        EditText userPW = findViewById(R.id.editTextNumberPassword);
+
+        String userNameStr = userName.getText().toString();
+        String userPWStr = userPW.getText().toString();
 
         //Chamar o onClickListener
         buttonLogin.setOnClickListener(view -> {
-            Toast toast = Toast.makeText(view.getContext(), R.string.login_successful, Toast.LENGTH_SHORT);
-            toast.show();
+            if(userNameStr.equals("")) {
+                Toast toast = Toast.makeText(view.getContext(), R.string.login_user_name_empty, Toast.LENGTH_SHORT);
+            }
+
+            if(userPWStr.equals("")) {
+                Toast toast = Toast.makeText(view.getContext(), R.string.login_user_pw_empty, Toast.LENGTH_SHORT);
+            }
         });
 
-        //Passar uma View.OnClickListener
-        //Implementar uma subclasse de View.OnClickListener
+
+    }
+
+    // Forma depreciada de implementação
+    public void forgotPW (View view) {
+        Toast toast = Toast.makeText(view.getContext(), R.string.login_button_forgot_pw_message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
